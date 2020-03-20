52b571a3254f83e0299bd1a0dfe6fb50d5a3f53f
Joseph Fischetti <fischettijw@gmail.com>
2020-03-20 16:16:10 -0400

    About to start CELL Class

--- FrmMinesweeper.cs
+++ FrmMinesweeper.cs
@@ -12,6 +12,9 @@ namespace cSharp___Minesweeper
 {
     public partial class FrmMinesweeper : Form
     {
+            static int clientWidth = 600;
+            static int clientHeight = 600;
+
         public FrmMinesweeper()
         {
             InitializeComponent();
@@ -19,7 +22,27 @@ namespace cSharp___Minesweeper
 
         private void FrmMinesweeper_Load(object sender, EventArgs e)
         {
+            CustomizeForm();
+        }
 
+
+        private void CustomizeForm()
+        {
+            this.Text = "Minesweeper   by Joseph Fischetti";
+
+            this.ClientSize = new Size(clientWidth, clientHeight);
+            this.MaximizeBox = false;
+            this.MaximumSize = new Size(this.Width, this.Height);
+            this.MinimizeBox = true;
+            this.MinimumSize = new Size(this.Width, this.Height);
+
+            //this.Icon = Properties.Resources.virus_image;
+
+            int sW = Screen.PrimaryScreen.WorkingArea.Width;
+            int sH = Screen.PrimaryScreen.WorkingArea.Height;
+            int fW = this.Width;
+            int fH = this.Height;
+            this.Location = new Point((sW - fW) / 2, (sH - fH) / 2);
         }
     }
 }
